d8221826337fe11f2689296092e4187cc3e0ba16
GloriaAcostaHeredia <60714360+GloriaAcostaHeredia@users.noreply.github.com>
2020-03-25 22:33:18 -0600

    Add files via upload

---
+++ alumnos.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sistema de Control de Alumnos</title>
+        <link rel="stylesheet" href="estilo.css">
+    </head>
+    <form action="conexion.php" method="post"/>
+    <body>
+    <center> <h1> Sistema de Control de Alumnos</h1></center>
+<p>Nombre: <input type="text" name="nombre" required/>   
+   e-mail: <input type="email" name="email" required/>
+   Matricula <input type="text" name="matricula" required>
+   Telefono de contacto: <input type="text"name="telefono" required></p>
+
+<p><center><input type="submit" value="Enviar!"> 
+   <input type="reset" value="borrar todo"></center></p>
+</form>
+</body>
+</html>
