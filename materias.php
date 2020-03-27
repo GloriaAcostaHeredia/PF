0ed99af9fe6869e4507d8d7b0b8e3a19d6b863db
GloriaAcostaHeredia <60714360+GloriaAcostaHeredia@users.noreply.github.com>
2020-03-27 16:26:02 -0600

    Add files via upload

---
+++ materias.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sistema de Control de Materias</title>
+        <link rel="stylesheet" href="estilo.css">
+    </head>
+    <form action="bdconnect.php" method="post"/>
+    <body>
+    <center> <h1> Sistema de Control de Materias</h1></center>
+<p>Nombre Del Grupo: <input type="text" name="nombre" required/>   
+   Clave: <input type="text"name="clave" required></p>
+
+<p><center><input type="submit" value="Enviar!"> 
+   <input type="reset" value="borrar todo"></center></p>
+</form>
+</body>
+</html>
