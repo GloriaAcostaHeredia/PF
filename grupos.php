8d1cfcb0265cc44b20bb2c611b372487431a09b3
jesusmartinez250201 <60726113+jesusmartinez250201@users.noreply.github.com>
2020-03-29 11:58:20 -0600

    Form de Grupos (Ultima Actuliz 29.03.2020)

--- grupos.php
+++ grupos.php
@@ -1,22 +1,56 @@
-
-<!DOCTYPE html>
 <html>
     <head>
-        <title>Sistema de Control de Alumnos</title>
+        <title>Sistema de Control de Grupos</title>
         <link rel="stylesheet" href="estilo.css">
+        <link rel="stylesheet" href="estilo2.css">
+        <meta charset = "UTF-8">
     </head>
-    <form action="bdconnect.php" method="post"/>
     <body>
-    <center> <h1> Sistema de Control de Alumnos</h1></center>
-<p>
-    Nombre Del grupo: <input type="text" class="input"name="nombre" required/>   
-   Clave de acceso: <input type="text"class="input"name="clave" required>
-</p>
-<div class="borde"> 
-            <center>
-            <input type="submit" class="submit" value="Enviar!">
-            <a class="boton" href="index.html"> Regresar </a> </center>
+        <h1 class="centro">Control de Grupos</h1>
+        <form action="grupos.php" method="post">
+        <div class="borde">
+        <p>Numero de Grupo:</p><input type="number" class="input" name="info[numero]" required/>
+        <p>Nombre de Grupo:</p><input type="text" class="input" name="info[nombre]" required/>
+        <br>
+        <div class="borde"> 
+            <input type="submit" class="submit" value="Guardar">
+            <br>
+            <a class="submit2" href="index.html">Volver</a>
         </div>
-</form>
+        </form>
+        <?php
+            require "dbconnect.php";
+            if ($conexion) {
+                if ($_POST) {
+                    $array = $_POST["info"];
+                    $nombre = $array["nombre"];
+                    $numero = $array["numero"];
+                    $sql = "INSERT INTO grupos(numgrupo, nombre) VALUES ('$numero', '$nombre')";
+                    $query = mysqli_query($conexion, $sql);
+                    if ($query) {
+                        ?>
+                        <br>
+                        <div class="exito">
+                        <h3>Datos Guardados Exitosamente!</h3>
+                        </div>
+                        <?php
+                    }else {
+                        ?>
+                        <br>
+                        <div class="error">
+                        <h3>Ha Ocurrido un error, comuniquese con el administrador</h3>
+                        </div>
+                        <?php
+                    }
+                }
+            }else {
+                ?>
+                <br>
+                <div class="error">
+                <h3>Ha Ocurrido un error, comuniquese con el administrador</h3>
+                </div>
+                <?php
+            }
+        ?>
 </body>
 </html>
