9f1e7c03d007ed183991989a03ef15b07902def0
jesusmartinez250201 <60726113+jesusmartinez250201@users.noreply.github.com>
2020-04-01 20:51:53 -0600

    Maestros (Ultima Actualiz 01.04.2020)

---
+++ maestros/borrar_maestros.php
@@ -0,0 +1,10 @@
+<?php
+//Incluir archivo de conexion a base de datos
+require '../dbconnect.php';
+    //Asignar el valor del id de la tabla mysql a una variable de php
+    $id = $_GET['id'];
+    //crear una consulta en mysql para eliminar los valores que esten en la misma fila que el id
+    $consulta = mysqli_query($conexion, "DELETE FROM maestros WHERE id='$id'");
+    //regresar a la tabla inicial de html
+    header('location: tab_maestros.php');
+?>
