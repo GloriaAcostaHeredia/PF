46bbcf1f08f7db24ea5b1b231b29e1182fde4990
jesusmartinez250201 <60726113+jesusmartinez250201@users.noreply.github.com>
2020-04-04 17:20:09 -0600

    Ultima Actualiz 04.04.2020

--- form_mod_carrera.php
+++ form_mod_carrera.php
@@ -16,7 +16,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 3 | Dashboard</title>
+    <title>AdminLTE 3 | Editar Carrera</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
